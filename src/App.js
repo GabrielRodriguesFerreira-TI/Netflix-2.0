8619d0a45f3a35907ecdd786cd8a09d99c1dce81
GabrielRf <ti.gabrielrf@gmail.com>
2023-01-26 16:31:31 -0300

    Feat: Sign up screen and user authentication with redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './pages/homeScreen/HomeScreen';
 import {
@@ -7,15 +7,41 @@ import {
   Route,
 } from "react-router-dom";
 import Login from './pages/login/Login';
+import { auth } from './services/firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
+      } else {
+        dispatch(logout)
+      }
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <div className='app'>
       <Router>
-        <Routes>
-          <Route path='/' element={<HomeScreen />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
+        {!user ? (
+          <Routes>
+            <Route path='/' element={<Login />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+          </Routes>
+        )}
       </Router>
     </div>
   );
