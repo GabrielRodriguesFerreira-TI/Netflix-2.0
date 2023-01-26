c8e86de071851683b5be1448aed7064a0aa8b9ff
GabrielRf <ti.gabrielrf@gmail.com>
2023-01-25 21:56:59 -0300

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCnyv8akVgQIUe3XhMNBtn3hWlB08jZDoU",
+    authDomain: "netflix-clone-38569.firebaseapp.com",
+    projectId: "netflix-clone-38569",
+    storageBucket: "netflix-clone-38569.appspot.com",
+    messagingSenderId: "38528642457",
+    appId: "1:38528642457:web:a4f9af0531dc9ba53db708"
+  };
